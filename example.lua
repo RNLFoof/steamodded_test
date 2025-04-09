ad8f70cad225c056bf132f558ac32ee992baa0a4
RNLFoof <z.zablotsky@gmail.com>
2025-04-09 00:55:36 -0400

    chore: removed the line number comments

--- example.lua
+++ example.lua
@@ -1,11 +1,15 @@
--- [yue]: ./libs/steamodded_test/example.yue
-local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))() -- 1
-local _obj_0 = G.steamodded_tests.tests -- 3
-_obj_0[#_obj_0 + 1] = testing.TestBundle("Example tests", { -- 5
-	testing.Test("Always passes", function() -- 5
-		return true -- 5
-	end), -- 5
-	testing.Test("Always fails", function() -- 6
-		return false -- 6
-	end) -- 6
-}) -- 3
+local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))()
+local _obj_0 = G.steamodded_tests.tests
+_obj_0[#_obj_0 + 1] = testing.TestBundle("Example tests", {
+	testing.Test("Always passes", function()
+		return true
+	end),
+	testing.Test("Always fails", function()
+		return false
+	end),
+	testing.TestBundle("Main game tests", {
+		testing.Test("Scoring an Ace", function()
+			return testing.create_state()
+		end)
+	})
+})
--- main.lua
+++ main.lua
@@ -1,130 +1,129 @@
--- [yue]: ./libs/steamodded_test/main.yue
-local _module_0 = { } -- 1
-local logger = { -- 3
-	log = print, -- 3
-	debug = print, -- 4
-	info = print, -- 5
-	warn = print, -- 6
-	error = print -- 7
-} -- 2
-local n_tabs -- 12
-n_tabs = function(indentation) -- 12
-	return string.rep("\t", indentation) -- 13
-end -- 12
-local Test -- 15
-do -- 15
-	local _class_0 -- 15
-	local _base_0 = { -- 15
-		run = function(self, indentation) -- 20
-			if indentation == nil then -- 20
-				indentation = 0 -- 20
-			end -- 20
-			local result = self:func() -- 21
-			if result == false then -- 22
-				logger.warn(n_tabs(indentation) .. "\tTest \"" .. tostring(self.name) .. "\" failed! :(") -- 23
-			end -- 22
-			return result -- 24
-		end -- 15
-	} -- 15
-	if _base_0.__index == nil then -- 15
-		_base_0.__index = _base_0 -- 15
-	end -- 24
-	_class_0 = setmetatable({ -- 15
-		__init = function(self, name, func) -- 16
-			self.name = name -- 17
-			self.func = func -- 18
-		end, -- 15
-		__base = _base_0, -- 15
-		__name = "Test" -- 15
-	}, { -- 15
-		__index = _base_0, -- 15
-		__call = function(cls, ...) -- 15
-			local _self_0 = setmetatable({ }, _base_0) -- 15
-			cls.__init(_self_0, ...) -- 15
-			return _self_0 -- 15
-		end -- 15
-	}) -- 15
-	_base_0.__class = _class_0 -- 15
-	Test = _class_0 -- 15
-end -- 24
-_module_0["Test"] = Test -- 15
-local TestBundle -- 27
-do -- 27
-	local _class_0 -- 27
-	local _base_0 = { -- 27
-		run = function(self, indentation) -- 32
-			if indentation == nil then -- 32
-				indentation = 0 -- 32
-			end -- 32
-			local tally = { -- 33
-				passed = 0, -- 33
-				failed = 0 -- 33
-			} -- 33
-			logger.info(n_tabs(indentation) .. "Running test bundle \"" .. tostring(self.name) .. "\"...") -- 34
-			for _, test in ipairs(self.tests) do -- 35
-				local result = test:run(indentation + 1) -- 36
-				local _update_0 = result and "passed" or "failed" -- 37
-				tally[_update_0] = tally[_update_0] + 1 -- 37
-			end -- 37
-			local all_passed = tally.failed == 0 -- 38
-			local via = all_passed and logger.info or logger.error -- 39
-			via(n_tabs(indentation) .. "Ran " .. tostring(#self.tests) .. " test(s). " .. tostring(tally.passed) .. " passed, " .. tostring(tally.failed) .. " failed.") -- 40
-			return all_passed -- 41
-		end -- 27
-	} -- 27
-	if _base_0.__index == nil then -- 27
-		_base_0.__index = _base_0 -- 27
-	end -- 41
-	_class_0 = setmetatable({ -- 27
-		__init = function(self, name, tests) -- 28
-			self.name = name -- 29
-			self.tests = tests -- 30
-		end, -- 27
-		__base = _base_0, -- 27
-		__name = "TestBundle" -- 27
-	}, { -- 27
-		__index = _base_0, -- 27
-		__call = function(cls, ...) -- 27
-			local _self_0 = setmetatable({ }, _base_0) -- 27
-			cls.__init(_self_0, ...) -- 27
-			return _self_0 -- 27
-		end -- 27
-	}) -- 27
-	_base_0.__class = _class_0 -- 27
-	TestBundle = _class_0 -- 27
-end -- 41
-_module_0["TestBundle"] = TestBundle -- 27
-local run_all_tests -- 44
-run_all_tests = function() -- 44
-	return G.steamodded_tests:run() -- 45
-end -- 44
-_module_0["run_all_tests"] = run_all_tests -- 46
-local init -- 47
-init = function() -- 47
-	if G.steamodded_tests == nil then -- 48
-		G.steamodded_tests = TestBundle("All tests", { }) -- 48
-	end -- 48
-end -- 47
-init() -- 49
-local create_state -- 52
-create_state = function(kwargs) -- 52
-	return G.FUNCS.start_run(nil, { -- 54
-		stake = 1 -- 54
-	}) -- 55
-end -- 52
-_module_0["create_state"] = create_state -- 55
-local success, dpAPI = pcall(require, "debugplus-api") -- 59
-if success and dpAPI.isVersionCompatible(1) then -- 60
-	local debugplus = dpAPI.registerID("steamodded_debug") -- 61
-	logger = debugplus.logger -- 62
-	debugplus.addCommand({ -- 65
-		name = "test", -- 65
-		shortDesc = "Testing command", -- 66
-		desc = "This command is an example to get you familar with how commands work", -- 67
-		exec = function(args, rawArgs, dp) -- 68
-			run_all_tests() -- 69
-			return "ok done ig" -- 70
-		end -- 68
-	}) -- 64
-end -- 60
-return _module_0 -- 71
+local _module_0 = { }
+local logger = {
+	log = print,
+	debug = print,
+	info = print,
+	warn = print,
+	error = print
+}
+local n_tabs
+n_tabs = function(indentation)
+	return string.rep("\t", indentation)
+end
+local Test
+do
+	local _class_0
+	local _base_0 = {
+		run = function(self, indentation)
+			if indentation == nil then
+				indentation = 0
+			end
+			local result = self:func()
+			if result == false then
+				logger.warn(n_tabs(indentation) .. "\tTest \"" .. tostring(self.name) .. "\" failed! :(")
+			end
+			return result
+		end
+	}
+	if _base_0.__index == nil then
+		_base_0.__index = _base_0
+	end
+	_class_0 = setmetatable({
+		__init = function(self, name, func)
+			self.name = name
+			self.func = func
+		end,
+		__base = _base_0,
+		__name = "Test"
+	}, {
+		__index = _base_0,
+		__call = function(cls, ...)
+			local _self_0 = setmetatable({ }, _base_0)
+			cls.__init(_self_0, ...)
+			return _self_0
+		end
+	})
+	_base_0.__class = _class_0
+	Test = _class_0
+end
+_module_0["Test"] = Test
+local TestBundle
+do
+	local _class_0
+	local _base_0 = {
+		run = function(self, indentation)
+			if indentation == nil then
+				indentation = 0
+			end
+			local tally = {
+				passed = 0,
+				failed = 0
+			}
+			logger.info(n_tabs(indentation) .. "Running test bundle \"" .. tostring(self.name) .. "\"...")
+			for _, test in ipairs(self.tests) do
+				local result = test:run(indentation + 1)
+				local _update_0 = result and "passed" or "failed"
+				tally[_update_0] = tally[_update_0] + 1
+			end
+			local all_passed = tally.failed == 0
+			local via = all_passed and logger.info or logger.error
+			via(n_tabs(indentation) .. "... done. Ran " .. tostring(#self.tests) .. " test(s). " .. tostring(tally.passed) .. " passed, " .. tostring(tally.failed) .. " failed.")
+			return all_passed
+		end
+	}
+	if _base_0.__index == nil then
+		_base_0.__index = _base_0
+	end
+	_class_0 = setmetatable({
+		__init = function(self, name, tests)
+			self.name = name
+			self.tests = tests
+		end,
+		__base = _base_0,
+		__name = "TestBundle"
+	}, {
+		__index = _base_0,
+		__call = function(cls, ...)
+			local _self_0 = setmetatable({ }, _base_0)
+			cls.__init(_self_0, ...)
+			return _self_0
+		end
+	})
+	_base_0.__class = _class_0
+	TestBundle = _class_0
+end
+_module_0["TestBundle"] = TestBundle
+local run_all_tests
+run_all_tests = function()
+	return G.steamodded_tests:run()
+end
+_module_0["run_all_tests"] = run_all_tests
+local init
+init = function()
+	if G.steamodded_tests == nil then
+		G.steamodded_tests = TestBundle("All tests", { })
+	end
+end
+init()
+local create_state
+create_state = function(kwargs)
+	return G.FUNCS.start_run(nil, {
+		stake = 1
+	})
+end
+_module_0["create_state"] = create_state
+local success, dpAPI = pcall(require, "debugplus-api")
+if success and dpAPI.isVersionCompatible(1) then
+	local debugplus = dpAPI.registerID("steamodded_test")
+	logger = debugplus.logger
+	debugplus.addCommand({
+		name = "test",
+		shortDesc = "Runs all tests.",
+		desc = "IDK man!!! It runs your tests and tells you the results!!",
+		exec = function(args, rawArgs, dp)
+			run_all_tests()
+			return "ok done ig"
+		end
+	})
+end
+return _module_0
