227327e33290524cc4136cb670044b53c62a6048
RNLFoof <z.zablotsky@gmail.com>
2025-04-09 21:32:31 -0400

    chore: tidied up the example tests a bit

--- example.lua
+++ example.lua
@@ -1,18 +1,20 @@
 local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))()
 local _obj_0 = G.steamodded_tests.tests
 _obj_0[#_obj_0 + 1] = testing.TestBundle("Example tests", {
-	testing.Test("Always passes", function()
-		return true
-	end),
-	testing.Test("Always fails", function()
-		return false
-	end),
-	testing.Test("Always confuses the tester", function()
-		return nil
-	end),
-	testing.Test("Always errors", function()
-		return assert(false)
-	end),
+	testing.TestBundle("Basic tests", {
+		testing.Test("Always passes", function()
+			return true
+		end),
+		testing.Test("Always fails", function()
+			return false
+		end),
+		testing.Test("Always confuses the tester", function()
+			return nil
+		end),
+		testing.Test("Always errors", function()
+			return assert(false)
+		end)
+	}),
 	testing.TestBundle("Main game tests", {
 		testing.Test("Scoring an Ace", {
 			testing.create_state_steps(),
