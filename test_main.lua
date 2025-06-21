45d22fefa4a1f7af7c2ee936ea8b7cad41ec07ef
RNLFoof <z.zablotsky@gmail.com>
2025-06-21 15:33:41 -0400

    chore: reformatted test file

--- test_main.lua
+++ test_main.lua
@@ -1,4 +1,6 @@
 local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))()
+local TestBundle = testing.TestBundle
+local Test = testing.Test
 local _anon_func_0 = function()
 	local _accum_0 = { }
 	local _len_0 = 1
@@ -22,104 +24,71 @@ local _anon_func_1 = function(G)
 	return _accum_0
 end
 local _obj_0 = G.steamodded_tests.tests
-_obj_0[#_obj_0 + 1] = testing.TestBundle("test tests", {
-	testing.TestBundle("assertions", {
-		testing.Test("assert_eq", function()
+_obj_0[#_obj_0 + 1] = TestBundle("test_tests", {
+	TestBundle("assertions", {
+		Test("assert_eq", function()
 			return testing.assert_eq(5, 5)
 		end),
-		testing.Test("assert_ne", function()
+		Test("assert_ne", function()
 			return testing.assert_ne(5, 4)
 		end),
-		testing.Test("assert_lt", function()
+		Test("assert_lt", function()
 			return testing.assert_lt(4, 5)
 		end),
-		testing.Test("assert_gt", function()
+		Test("assert_gt", function()
 			return testing.assert_gt(5, 4)
 		end),
-		testing.Test("assert_le", function()
+		Test("assert_le", function()
 			return testing.assert_le(4, 5)
 		end),
-		testing.Test("assert_ge", function()
+		Test("assert_ge", function()
 			return testing.assert_ge(5, 4)
 		end),
-		testing.Test("assert_le", function()
+		Test("assert_le", function()
 			return testing.assert_le(5, 5)
 		end),
-		testing.Test("assert_ge", function()
+		Test("assert_ge", function()
 			return testing.assert_ge(5, 5)
 		end)
 	}),
-	testing.TestBundle("add_centers", {
-		testing.Test("1", (function()
-			local _tab_0 = { }
-			local _obj_1 = testing.create_state_steps()
-			local _idx_0 = 1
-			for _key_0, _value_0 in pairs(_obj_1) do
-				if _idx_0 == _key_0 then
-					_tab_0[#_tab_0 + 1] = _value_0
-					_idx_0 = _idx_0 + 1
-				else
-					_tab_0[_key_0] = _value_0
-				end
-			end
-			_tab_0[#_tab_0 + 1] = function()
+	TestBundle("add_centers", {
+		Test("1", {
+			testing.create_state_steps(),
+			function()
 				return testing.add_centers({
 					"j_joker"
 				})
-			end
-			_tab_0[#_tab_0 + 1] = function()
+			end,
+			function()
 				return testing.assert_eq(G.jokers.cards[1].config.center.key, "j_joker")
 			end
-			return _tab_0
-		end)()),
-		testing.Test("2", (function()
-			local _tab_0 = { }
-			local _obj_1 = testing.create_state_steps()
-			local _idx_0 = 1
-			for _key_0, _value_0 in pairs(_obj_1) do
-				if _idx_0 == _key_0 then
-					_tab_0[#_tab_0 + 1] = _value_0
-					_idx_0 = _idx_0 + 1
-				else
-					_tab_0[_key_0] = _value_0
-				end
-			end
-			_tab_0[#_tab_0 + 1] = function()
+		}),
+		Test("2", {
+			testing.create_state_steps(),
+			function()
 				return testing.add_centers({
 					"j_joker",
 					"j_joker"
 				})
-			end
-			_tab_0[#_tab_0 + 1] = function()
+			end,
+			function()
 				return testing.assert_eq(G.jokers.cards[1].config.center.key, "j_joker")
-			end
-			_tab_0[#_tab_0 + 1] = function()
+			end,
+			function()
 				return testing.assert_eq(G.jokers.cards[2].config.center.key, "j_joker")
 			end
-			return _tab_0
-		end)()),
-		testing.Test("6, sequential", (function()
-			local _tab_0 = { }
-			local _obj_1 = testing.create_state_steps()
-			local _idx_0 = 1
-			for _key_0, _value_0 in pairs(_obj_1) do
-				if _idx_0 == _key_0 then
-					_tab_0[#_tab_0 + 1] = _value_0
-					_idx_0 = _idx_0 + 1
-				else
-					_tab_0[_key_0] = _value_0
-				end
-			end
-			_tab_0[#_tab_0 + 1] = function()
+		}),
+		Test("6_sequential", {
+			testing.create_state_steps(),
+			function()
 				return testing.add_centers(_anon_func_0())
-			end
-			_tab_0[#_tab_0 + 1] = function()
+			end,
+			function()
 				return testing.assert_eq(#_anon_func_1(G), 6)
-			end
-			_tab_0[#_tab_0 + 1] = function()
+			end,
+			function()
 				return testing.assert_eq(#SMODS.find_card("j_joker"), 6)
 			end
-			return _tab_0
-		end)())
+		})
 	})
 })
